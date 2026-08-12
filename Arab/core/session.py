8857fb108e7ac0c79dcf871a306943842755acdd
alrsasbwd37-ai <alrsasbwd37@gmail.com>
2026-08-12 15:39:05 +0300

    Update session.py

--- Arab/core/session.py
+++ Arab/core/session.py
@@ -2,11 +2,25 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 from Arab.Config import Config
 
+print("DEBUG SESSION FILE")
+print("APP_ID =", Config.APP_ID)
+print("APP_HASH =", Config.APP_HASH)
+print("SESSION =", bool(Config.STRING_SESSION))
 
-session = StringSession(Config.STRING_SESSION)
+if not Config.APP_ID or not Config.APP_HASH:
+    raise Exception("API_ID OR API_HASH EMPTY")
 
 iqthon = TelegramClient(
-    session,
+    StringSession(Config.STRING_SESSION),
     Config.APP_ID,
     Config.APP_HASH
 )
+
+if Config.BOT_TOKEN:
+    iqthon.tgbot = TelegramClient(
+        "bot",
+        Config.APP_ID,
+        Config.APP_HASH
+    )
+else:
+    iqthon.tgbot = None
