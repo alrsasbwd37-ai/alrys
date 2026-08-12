feda3a237b9a8d89a2e3ff70e85f97716b12eaa1
alrsasbwd37-ai <alrsasbwd37@gmail.com>
2026-08-12 13:37:47 +0300

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -10,13 +10,5 @@ def home():
 def health():
     return "OK", 200
 
-def start_web():
-    app.run(
-        host="0.0.0.0",
-        port=10000,
-        debug=False,
-        threaded=True
-    )
-
 if __name__ == "__main__":
-    start_web()
+    app.run(host="0.0.0.0", port=10000, debug=False, threaded=True)
